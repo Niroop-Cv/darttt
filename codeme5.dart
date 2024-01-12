6fe76b0327f2f23c9a5a9cbadbf0c2d15feab6f7
Niroop Cv <141113159+Niroop-Cv@users.noreply.github.com>
2024-01-12 21:06:04 +0530

    Add files via upload

---
+++ codeme5.dart
@@ -0,0 +1,45 @@
+// void main() {
+//   List<int> numbers = [5, 2, 9, 1, 5, 6, 3, 8];
+
+//   int max = numbers[0];
+//   int min = numbers[0];
+
+//   for (int i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > max) {
+//       max = numbers[i];
+//     }
+//     if (numbers[i] < min) {
+//       min = numbers[i];
+//     }
+//   }
+
+//   print("Maximum element: $max");
+//   print("Minimum element: $min");
+// }
+
+
+
+// void main(){
+//   var abc=[10,20,30];
+//   var def=[5,6,7];
+//   var newList=[abc,...def];
+//   print(newList);
+
+// }
+
+
+
+import 'dart:io';
+
+// void main(){
+//   print("enter the number");
+// int number=int.parse(stdin.readLineSync()!);
+// for(int i=1;i<=number;i++){
+//   for(int j=1;j<=i;j++){
+//     stdout.write("* ");
+//   }
+//   print("\n");
+// }
+
+
+//}
